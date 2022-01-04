86b01e8bc60fddc11bf224666f977ce67c7c65de
Saifurrehmanbaloch <surrehman180@gmail.com>
2022-01-04 09:56:29 +0500

    added the files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ import React, { useState } from 'react';
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
 
+const removeBodyClasses = ()=>{
+  document.body.classList.remove('bg-light')
+  document.body.classList.remove('bg-dark')
+  document.body.classList.remove('bg-warning')
+  document.body.classList.remove('bg-danger')
+  document.body.classList.remove('bg-success')
+}
+
   const toggleMode = (cls)=>{
+    removeBodyClasses();
+    console.log(cls);
+    document.body.classList.add('bg-'+cls)
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -21,10 +21,13 @@ export default function Navbar(props) {
             </li>
           </ul>
           <div className="d-flex">
-            <div className="bg-primary rounded mx-2" onClick={props.toggleMode} style={{height: '30px', width:'30px'}}></div>
+            <div className="bg-primary rounded mx-2" onClick={()=>{props.toggleMode('primary')}} style={{height: '30px', width:'30px', cursor: 'pointer'}}></div>
+            <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}} style={{height: '30px', width:'30px', cursor: 'pointer'}}></div>
+            <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode('success')}} style={{height: '30px', width:'30px', cursor: 'pointer'}}></div>
+            <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('warning')}} style={{height: '30px', width:'30px', cursor: 'pointer'}}></div>
           </div>
           <div className={`form-check form-switch text-${props.mode===`light`?`dark`:`light`}`}>
-            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
+            <input className="form-check-input" onClick={()=>{props.toggleMode(null)}} type="checkbox" id="flexSwitchCheckDefault" />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Toggle DarkMode</label>
           </div>
         </div>
