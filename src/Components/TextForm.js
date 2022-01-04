a88abd1cda9f25bffc53ec03ce8d90e43c2df8f8
Saifurrehmanbaloch <surrehman180@gmail.com>
2022-01-04 10:10:45 +0500

    update TextForm.js

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -3,12 +3,10 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText)
     }
     const handleLoClick = ()=>{
-        // console.log("Uppercase was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText)
     }
@@ -19,7 +17,6 @@ export default function TextForm(props) {
     }
 
     const handleOnChange = (event)=>{
-        // console.log("On change");
         setText(event.target.value)
     }
 
@@ -55,7 +52,7 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your text summary</h2>
-            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Nothing to preview!"}</p>
